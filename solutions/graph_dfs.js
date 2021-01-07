77c72288287450e422796221aaff27ea591d9abe
Rupa <rupa211984@gmail.com>
2021-01-06 16:07:36 -0800

    Add solution explanations

--- solutions/graph_dfs.js
+++ solutions/graph_dfs.js
@@ -3,12 +3,11 @@ function isPath(graph, vertexA, vertexB, visited = new Set()) {
     return true;
   }
 
-  const hasVisited = new Set(visited);
-  hasVisited.add(vertexA);
+  visited.add(vertexA);
 
   for (const vertex of graph[vertexA]) {
-    if (!hasVisited.has(vertex)) {
-      if (isPath(graph, vertex, vertexB, hasVisited)) {
+    if (!visited.has(vertex)) {
+      if (isPath(graph, vertex, vertexB, visited)) {
         return true;
       }
     }
@@ -87,4 +86,41 @@ if (require.main === module) {
 module.exports = isPath;
 
 // Please add your pseudocode to this file
+/****************************************************************************************** 
+ * function is_path(graph, vertex_a, vertex_b, visited):
+ *  return true if vertex_b exists in vertex_a adjacency list
+ * 
+ *  add vertex_a to visited
+ * 
+ *  iterate over each vertex in vertex_a adjacency list:
+ *    if vertex is not in visited:
+ *      initialize variable result to value returned from is_path(graph, vertex, vertex_b, visited)
+ *      return true if result is true
+ * 
+ *  return false
+ * *****************************************************************************************/
+
 // And a written explanation of your solution
+/****************************************************************************************** 
+ * I started by thinking of my base case, which is that the algorithm should return true if 
+ * vertex B is in vertex A's adjacency list. I'm not checking for inclusion in the other direction
+ * from B to A because this is a directed graph. Since it is possible for there to be loops in 
+ * the graph, I need to also store a list of vertices that have been visited. This prevents me 
+ * from creating a stack overflow. 
+ * 
+ * I chose a Set instead of an Array because checking if a vertex
+ * has been visited already would then have an O(1) lookup time, whereas an Array would be O(n). 
+ * The trade-off is that the Set takes up more space in memory. 
+ * 
+ * I chose to iterate over the adjacency list using a loop that can be broken out of, as opposed
+ * to one that offers no breaking mechanism. I did this because I want iteration to stop as soon
+ * as a path has been found. At that time I can simply return true.
+ * 
+ * On each iteration, I check if the vertex in the adjacency list has not been visited. If it hasn't
+ * I recurse with the current vertex that's being iterated over, vertex B, and the  
+ * visited Set. If the recursive call returns true, that value will travel up the stack and
+ * continue returning true until it reaches the top and the whole method returns true.
+ * 
+ * If all reachable vertices are visited and vertex B is not found, the algorithm will return
+ * false once it exits the iteration in each frame.
+ * *****************************************************************************************/
--- solutions/graph_dfs.rb
+++ solutions/graph_dfs.rb
@@ -3,16 +3,14 @@ require "set"
 def is_path(graph, vertex_a, vertex_b, visited = Set.new)
   return true if graph[vertex_a].include?(vertex_b)
 
-  has_visited = Set.new(visited)
-  has_visited.add(vertex_a)
-
+  visited.add(vertex_a)
   idx = 0
 
   while idx < graph[vertex_a].length
     vertex = graph[vertex_a][idx]
     idx += 1
-    next if has_visited.include?(vertex)
-    return true if is_path(graph, vertex, vertex_b, has_visited)
+    next if visited.include?(vertex)
+    return true if is_path(graph, vertex, vertex_b, visited)
   end
 
   false
@@ -84,4 +82,41 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+###################################################################################################
+ # function is_path(graph, vertex_a, vertex_b, visited):
+ #  return true if vertex_b exists in vertex_a adjacency list
+ # 
+ #  add vertex_a to visited
+ # 
+ #  iterate over each vertex in vertex_a adjacency list:
+ #    if vertex is not in visited:
+ #      initialize variable result to value returned from is_path(graph, vertex, vertex_b, visited)
+ #      return true if result is true
+ # 
+ #  return false
+###################################################################################################
+
 # And a written explanation of your solution
+###################################################################################################
+ # I started by thinking of my base case, which is that the algorithm should return true if 
+ # vertex B is in vertex A's adjacency list. I'm not checking for inclusion in the other direction
+ # from B to A because this is a directed graph. Since it is possible for there to be loops in 
+ # the graph, I need to also store a list of vertices that have been visited. This prevents me 
+ # from creating a stack overflow. 
+ # 
+ # I chose a Set instead of an Array because checking if a vertex
+ # has been visited already would then have an O(1) lookup time, whereas an Array would be O(n). 
+ # The trade-off is that the Set takes up more space in memory. 
+ # 
+ # I chose to iterate over the adjacency list using a loop that can be broken out of, as opposed
+ # to one that offers no breaking mechanism. I did this because I want iteration to stop as soon
+ # as a path has been found. At that time I can simply return true.
+ # 
+ # On each iteration, I check if the vertex in the adjacency list has not been visited. If it hasn't
+ # I recurse with the current vertex that's being iterated over, vertex B, and the  
+ # visited Set. If the recursive call returns true, that value will travel up the stack and
+ # continue returning true until it reaches the top and the whole method returns true.
+ # 
+ # If all reachable vertices are visited and vertex B is not found, the algorithm will return
+ # false once it exits the iteration in each frame.
+###################################################################################################
